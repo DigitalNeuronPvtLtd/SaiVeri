16258c7b3781edea4fc82407f17628226c5d75b8
root <spsarolkar@gmail.com>
2011-03-19 08:59:56 +0530

    Address Control

---
+++ AddressControl/AddressControl.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace SaiVeri
+{
+    partial class AddressControl
+    {
+        
+        
+        private string  addressName;
+
+        public string getAddressName() {
+            return addressName;
+        }
+
+        public void setAddressName(string addressName) {
+            this.addressName = addressName;
+        }
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_addressline_1 = new System.Windows.Forms.Label();
+            this.txt_address1 = new System.Windows.Forms.TextBox();
+            this.lbl_address2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_area = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.lbl_landmark = new System.Windows.Forms.Label();
+            this.txtLandmark = new System.Windows.Forms.TextBox();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.lbl_taluka = new System.Windows.Forms.Label();
+            this.txtTaluka = new System.Windows.Forms.TextBox();
+            this.lbl_district = new System.Windows.Forms.Label();
+            this.txt_district = new System.Windows.Forms.TextBox();
+            this.lbl_state = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbl_pin = new System.Windows.Forms.Label();
+            this.txt_pin = new System.Windows.Forms.TextBox();
+            this.lbl_areaCode = new System.Windows.Forms.Label();
+            this.txt_areaCode = new System.Windows.Forms.TextBox();
+            this.lbl_phone1 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbl_phone2 = new System.Windows.Forms.Label();
+            this.txtPhone2 = new System.Windows.Forms.TextBox();
+            this.lbl_mobile = new System.Windows.Forms.Label();
+            this.txt_mobile = new System.Windows.Forms.TextBox();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_addressline_1
+            // 
+            this.lbl_addressline_1.AutoSize = true;
+            this.lbl_addressline_1.Location = new System.Drawing.Point(4, 4);
+            this.lbl_addressline_1.Name = "lbl_addressline_1";
+            this.lbl_addressline_1.Size = new System.Drawing.Size(103, 17);
+            this.lbl_addressline_1.TabIndex = 0;
+            this.lbl_addressline_1.Text = "Address Line 1";
+            // 
+            // txt_address1
+            // 
+            this.txt_address1.Location = new System.Drawing.Point(133, 1);
+            this.txt_address1.Name = "txt_address1";
+            this.txt_address1.Size = new System.Drawing.Size(277, 22);
+            this.txt_address1.TabIndex = 1;
+            // 
+            // lbl_address2
+            // 
+            this.lbl_address2.AutoSize = true;
+            this.lbl_address2.Location = new System.Drawing.Point(4, 32);
+            this.lbl_address2.Name = "lbl_address2";
+            this.lbl_address2.Size = new System.Drawing.Size(103, 17);
+            this.lbl_address2.TabIndex = 2;
+            this.lbl_address2.Text = "Address Line 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lbl_area
+            // 
+            this.lbl_area.AutoSize = true;
+            this.lbl_area.Location = new System.Drawing.Point(69, 63);
+            this.lbl_area.Name = "lbl_area";
+            this.lbl_area.Size = new System.Drawing.Size(38, 17);
+            this.lbl_area.TabIndex = 4;
+            this.lbl_area.Text = "Area";
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(133, 60);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(100, 22);
+            this.txtArea.TabIndex = 5;
+            // 
+            // lbl_landmark
+            // 
+            this.lbl_landmark.AutoSize = true;
+            this.lbl_landmark.Location = new System.Drawing.Point(257, 63);
+            this.lbl_landmark.Name = "lbl_landmark";
+            this.lbl_landmark.Size = new System.Drawing.Size(71, 17);
+            this.lbl_landmark.TabIndex = 6;
+            this.lbl_landmark.Text = "Landmark";
+            // 
+            // txtLandmark
+            // 
+            this.txtLandmark.Location = new System.Drawing.Point(334, 60);
+            this.txtLandmark.Name = "txtLandmark";
+            this.txtLandmark.Size = new System.Drawing.Size(100, 22);
+            this.txtLandmark.TabIndex = 7;
+            // 
+            // lbl_city
+            // 
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Location = new System.Drawing.Point(458, 63);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(31, 17);
+            this.lbl_city.TabIndex = 8;
+            this.lbl_city.Text = "City";
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(506, 60);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(100, 22);
+            this.txt_city.TabIndex = 9;
+            // 
+            // lbl_taluka
+            // 
+            this.lbl_taluka.AutoSize = true;
+            this.lbl_taluka.Location = new System.Drawing.Point(646, 63);
+            this.lbl_taluka.Name = "lbl_taluka";
+            this.lbl_taluka.Size = new System.Drawing.Size(51, 17);
+            this.lbl_taluka.TabIndex = 10;
+            this.lbl_taluka.Text = "Taluka";
+            // 
+            // txtTaluka
+            // 
+            this.txtTaluka.Location = new System.Drawing.Point(703, 60);
+            this.txtTaluka.Name = "txtTaluka";
+            this.txtTaluka.Size = new System.Drawing.Size(100, 22);
+            this.txtTaluka.TabIndex = 11;
+            // 
+            // lbl_district
+            // 
+            this.lbl_district.AutoSize = true;
+            this.lbl_district.Location = new System.Drawing.Point(831, 63);
+            this.lbl_district.Name = "lbl_district";
+            this.lbl_district.Size = new System.Drawing.Size(51, 17);
+            this.lbl_district.TabIndex = 12;
+            this.lbl_district.Text = "District";
+            // 
+            // txt_district
+            // 
+            this.txt_district.Location = new System.Drawing.Point(897, 60);
+            this.txt_district.Name = "txt_district";
+            this.txt_district.Size = new System.Drawing.Size(100, 22);
+            this.txt_district.TabIndex = 13;
+            // 
+            // lbl_state
+            // 
+            this.lbl_state.AutoSize = true;
+            this.lbl_state.Location = new System.Drawing.Point(1028, 63);
+            this.lbl_state.Name = "lbl_state";
+            this.lbl_state.Size = new System.Drawing.Size(41, 17);
+            this.lbl_state.TabIndex = 14;
+            this.lbl_state.Text = "State";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1085, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 15;
+            // 
+            // lbl_pin
+            // 
+            this.lbl_pin.AutoSize = true;
+            this.lbl_pin.Location = new System.Drawing.Point(72, 98);
+            this.lbl_pin.Name = "lbl_pin";
+            this.lbl_pin.Size = new System.Drawing.Size(28, 17);
+            this.lbl_pin.TabIndex = 16;
+            this.lbl_pin.Text = "Pin";
+            // 
+            // txt_pin
+            // 
+            this.txt_pin.Location = new System.Drawing.Point(133, 98);
+            this.txt_pin.Name = "txt_pin";
+            this.txt_pin.Size = new System.Drawing.Size(100, 22);
+            this.txt_pin.TabIndex = 17;
+            // 
+            // lbl_areaCode
+            // 
+            this.lbl_areaCode.AutoSize = true;
+            this.lbl_areaCode.Location = new System.Drawing.Point(253, 98);
+            this.lbl_areaCode.Name = "lbl_areaCode";
+            this.lbl_areaCode.Size = new System.Drawing.Size(75, 17);
+            this.lbl_areaCode.TabIndex = 18;
+            this.lbl_areaCode.Text = "Area Code";
+            // 
+            // txt_areaCode
+            // 
+            this.txt_areaCode.Location = new System.Drawing.Point(334, 98);
+            this.txt_areaCode.Name = "txt_areaCode";
+            this.txt_areaCode.Size = new System.Drawing.Size(100, 22);
+            this.txt_areaCode.TabIndex = 19;
+            // 
+            // lbl_phone1
+            // 
+            this.lbl_phone1.AutoSize = true;
+            this.lbl_phone1.Location = new System.Drawing.Point(440, 98);
+            this.lbl_phone1.Name = "lbl_phone1";
+            this.lbl_phone1.Size = new System.Drawing.Size(61, 17);
+            this.lbl_phone1.TabIndex = 20;
+            this.lbl_phone1.Text = "Phone 1";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(506, 98);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone.TabIndex = 21;
+            // 
+            // lbl_phone2
+            // 
+            this.lbl_phone2.AutoSize = true;
+            this.lbl_phone2.Location = new System.Drawing.Point(636, 98);
+            this.lbl_phone2.Name = "lbl_phone2";
+            this.lbl_phone2.Size = new System.Drawing.Size(61, 17);
+            this.lbl_phone2.TabIndex = 22;
+            this.lbl_phone2.Text = "Phone 2";
+            // 
+            // txtPhone2
+            // 
+            this.txtPhone2.Location = new System.Drawing.Point(703, 98);
+            this.txtPhone2.Name = "txtPhone2";
+            this.txtPhone2.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone2.TabIndex = 23;
+            // 
+            // lbl_mobile
+            // 
+            this.lbl_mobile.AutoSize = true;
+            this.lbl_mobile.Location = new System.Drawing.Point(834, 98);
+            this.lbl_mobile.Name = "lbl_mobile";
+            this.lbl_mobile.Size = new System.Drawing.Size(49, 17);
+            this.lbl_mobile.TabIndex = 24;
+            this.lbl_mobile.Text = "Mobile";
+            // 
+            // txt_mobile
+            // 
+            this.txt_mobile.Location = new System.Drawing.Point(897, 98);
+            this.txt_mobile.Name = "txt_mobile";
+            this.txt_mobile.Size = new System.Drawing.Size(100, 22);
+            this.txt_mobile.TabIndex = 25;
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Location = new System.Drawing.Point(1031, 98);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(42, 17);
+            this.lbl_email.TabIndex = 26;
+            this.lbl_email.Text = "Email";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(1085, 98);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(100, 22);
+            this.txt_email.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AddressControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.txt_mobile);
+            this.Controls.Add(this.lbl_mobile);
+            this.Controls.Add(this.txtPhone2);
+            this.Controls.Add(this.lbl_phone2);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lbl_phone1);
+            this.Controls.Add(this.txt_areaCode);
+            this.Controls.Add(this.lbl_areaCode);
+            this.Controls.Add(this.txt_pin);
+            this.Controls.Add(this.lbl_pin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbl_state);
+            this.Controls.Add(this.txt_district);
+            this.Controls.Add(this.lbl_district);
+            this.Controls.Add(this.txtTaluka);
+            this.Controls.Add(this.lbl_taluka);
+            this.Controls.Add(this.txt_city);
+            this.Controls.Add(this.lbl_city);
+            this.Controls.Add(this.txtLandmark);
+            this.Controls.Add(this.lbl_landmark);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.lbl_area);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_address2);
+            this.Controls.Add(this.txt_address1);
+            this.Controls.Add(this.lbl_addressline_1);
+            this.Name = "AddressControl";
+            this.Size = new System.Drawing.Size(1230, 123);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_addressline_1;
+        private System.Windows.Forms.TextBox txt_address1;
+        private System.Windows.Forms.Label lbl_address2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_area;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label lbl_landmark;
+        private System.Windows.Forms.TextBox txtLandmark;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.Label lbl_taluka;
+        private System.Windows.Forms.TextBox txtTaluka;
+        private System.Windows.Forms.Label lbl_district;
+        private System.Windows.Forms.TextBox txt_district;
+        private System.Windows.Forms.Label lbl_state;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_pin;
+        private System.Windows.Forms.TextBox txt_pin;
+        private System.Windows.Forms.Label lbl_areaCode;
+        private System.Windows.Forms.TextBox txt_areaCode;
+        private System.Windows.Forms.Label lbl_phone1;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lbl_phone2;
+        private System.Windows.Forms.TextBox txtPhone2;
+        private System.Windows.Forms.Label lbl_mobile;
+        private System.Windows.Forms.TextBox txt_mobile;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Button button1;
+    }
+}
